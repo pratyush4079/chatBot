2969490ea14ffcea310755319648d986b3f550f9
Pratyush1190 <pratyush.ranjan@williamslea.com>
2025-08-20 12:26:38 +0530

    added yaml

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 uvicorn main:app --host 0.0.0.0 --port $PORT
+services:
+  - type: web
+    name: fastapi-webhook
+    env: python
+    buildCommand: ""
+    startCommand: ./start.sh
+    envVars:
+      - key: PYTHON_VERSION
+        value: 3.13
+    plan: free
